2fd897f2337a4e256e163f040f3b7a1401e16360
anas1980new <114800243+anas1980new@users.noreply.github.com>
2022-11-16 04:51:32 +0400

    Update Helmet_Detection_App.py

--- Helmet_Detection_App.py
+++ Helmet_Detection_App.py
@@ -267,7 +267,7 @@ elif app_mode == 'Detect an Image':
 
     img_file_buffer = st.sidebar.file_uploader("Upload an Image", type=["JPG", "jpeg", "png"])
     if img_file_buffer is not None:
-        resized = cv2.resize(img_file_buffer, (640,640), interpolation= cv2.INTER_NEAREST)
+        resized = cv2.resize(DEMO_IMAGE, (640,640), interpolation= cv2.INTER_NEAREST)
         image = np.array(Image.open(resized))
         feedback1="Uploaded Image: "
         st.sidebar.image(image)
